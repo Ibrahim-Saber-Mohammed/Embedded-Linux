06794bc6537c1c2bc0bf2c4b0130b25f9919ce39
Ibrahim-Saber-Mohammed <ibrahimsaber249@gmail.com>
2024-05-24 19:12:25 +0300

    commit -m "feat: Adding Function overloading to handle Zero Arguments test: Added Test cases "

---
+++ CppIntermediateProject/build/Test/CMakeFiles/APPLICATION_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "APPLICATION_TEST"
+  "APPLICATION_TEST.pdb"
+  "APPLICATION_TEST[1]_tests.cmake"
+  "CMakeFiles/APPLICATION_TEST.dir/CustomMathTest.cpp.o"
+  "CMakeFiles/APPLICATION_TEST.dir/CustomMathTest.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APPLICATION_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CppIntermediateProject/build/Test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibrahim/Desktop/Cpp/CustomMathLib/CppIntermediateProject/Test
+# Build directory: /home/ibrahim/Desktop/Cpp/CustomMathLib/CppIntermediateProject/build/Test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/ibrahim/Desktop/Cpp/CustomMathLib/CppIntermediateProject/build/Test/APPLICATION_TEST[1]_include.cmake")
